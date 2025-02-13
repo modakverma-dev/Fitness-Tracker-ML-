19476706797874122c1b322f8c8dd086bb016d32
modakverma-tmpl <modak.v@tunica.tech>
2025-02-13 22:52:43 +0530

    first commit

---
+++ src/data/make_dataset.py
@@ -0,0 +1,6 @@
+import pandas as pd
+# from glob import glob
+
+single_file_acc = pd.read_csv(
+    "../../data/raw/MetaMotion/A-bench-heavy_MetaWear_2019-01-14T14.22.49.165_C42732BE255C_Accelerometer_12.500Hz_1.4.4.csv"
+)
