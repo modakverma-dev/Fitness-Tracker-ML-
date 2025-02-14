102fc92b04aea3e913589006a954104c5fce91e3
modakverma-tmpl <modak.v@tunica.tech>
2025-02-14 21:42:23 +0530

    preprocessing updates

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -11,3 +11,40 @@ single_file_gyr = pd.read_csv(
 
 files = glob("../../data/raw/MetaMotion/*.csv")
 len(files)
+
+data_path = "../../data/raw/MetaMotion/"
+f = files[0]
+participants = f.split("-")[0].replace(data_path, "")
+label = f.split("-")[1]
+category = f.split("-")[2].rstrip("123")
+
+df = pd.read_csv(f)
+
+df["participants"] = participants
+df["label"] = label
+df["category"] = category
+
+acc_df = pd.DataFrame()
+gyr_df = pd.DataFrame()
+
+acc_set = 1
+gyr_set = 1
+
+for f in files:
+    participants = f.split("-")[0].replace(data_path, "")
+    label = f.split("-")[1]
+    category = f.split("-")[2].rstrip("123").rstrip("MetaWear_2019")
+    df = pd.read_csv(f)
+    df["participants"] = participants
+    df["label"] = label
+    df["category"] = category
+    if "Accelerometer" in f:
+        df["set"] = acc_set
+        acc_set += 1
+        acc_df = pd.concat([acc_df, df])
+    if "Gyroscope" in f:
+        df["set"] = gyr_set
+        gyr_set += 1
+        gyr_df = pd.concat([gyr_df, df])
+
+acc_df[acc_df["set"] == 10]
